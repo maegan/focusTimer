3674d125738fec577d8f3b748754174219cc5ca5
maegan <maegan@siena.edu>
2024-02-23 12:24:43 -0500

    added state

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { colors } from "../utils/colors";
 import { TextInput } from "react-native-paper";
 
-export const Focus = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Focus Feature</Text>
-  </View>
-);
+export const Focus = () => {
+  const [subject, setSubject] = useState(null);
+  console.log(subject);
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={setSubject}
+          label="What would you like to focus on?"
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  text: {
-    color: colors.offWhite,
+  inputContainer: {
+    flex: 1,
+    padding: 25,
+    justifyContent: "top",
   },
 });
