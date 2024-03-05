14e5cb4b02fc6cc8b0180636b7b7134c0b245520
maegan <maegan@siena.edu>
2024-03-05 17:12:00 -0500

    end of 3/5

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Countdown } from "../components/Countdown";
+import { colors } from "../utils/colors";
+import { fontSizes } from "../utils/sizes.js";
 
 export const Timer = ({ focusSubject }) => (
   <View style={styles.container}>
     <View style={styles.countdown}>
       <Countdown isPaused onProgress={() => {}} />
     </View>
-    <Text style={{ color: "white" }}>Focus Feature: {focusSubject}</Text>
+    <Text style={styles.title}>Focus Feature: </Text>
+    <Text style={styles.task}>{focusSubject}</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  countdown: {
+    flex: 0.3,
+    aligntItems: "center",
+    backgroundColor: "yellow",
+  },
+  title: {
+    color: colors.offWhite,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: fontSizes.xl,
+  },
+  task: {
+    color: colors.offWhite,
+    textAlign: "center",
+    fontSize: fontSizes.lg,
   },
 });
